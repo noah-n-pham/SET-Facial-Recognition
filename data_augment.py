b9cff12a808a59b7c83c8380bbe0c57f08da1c4b
Tyler Bullock <tylerbullock@ufl.edu>
2025-10-28 20:48:53 -0400

    commented out augment code, began adding resnet-18 training

--- data_augment.py
+++ data_augment.py
@@ -1,9 +1,12 @@
 import os
 import sys
 import torch
+from torchvision import datasets, transforms, models
+from torch import nn, optim  # optim for adam
 import albumentations as A
 import cv2
 import numpy as np
+import shutil
 
 nameList = ["tyler", "ben", "james", "rishab",
             "noah", "joyce", "nate", "janav", "hoek"]
@@ -23,29 +26,38 @@ transform = A.Compose([
     # A.Normalize(mean=[0.5, 0.5,0.5],std=[0.5,0.5, 0.5]),
 ])
 
-count = 0
-for name in nameList:
-    count = 0
-    for i in range(0, 100, 5):
-        image = cv2.imread("Dataset/"+name+"/"+name+"_"+str(i)+".png")
-        augmented_image = transform(image=image)["image"]
-        augmented_image2 = transform(image=image)["image"]
-        augmented_image3 = transform(image=image)["image"]
-        augmented_image4 = transform(image=image)["image"]
-        augmented_image5 = transform(image=image)["image"]
 
-        cv2.imwrite(
-            f"AlbumentationAugments/{name}/{name}_augmented_{count}.png", augmented_image)
-        count += 1
-        cv2.imwrite(
-            f"AlbumentationAugments/{name}/{name}_augmented_{count}.png", augmented_image2)
-        count += 1
-        cv2.imwrite(
-            f"AlbumentationAugments/{name}/{name}_augmented_{count}.png", augmented_image3)
-        count += 1
-        cv2.imwrite(
-            f"AlbumentationAugments/{name}/{name}_augmented_{count}.png", augmented_image4)
-        count += 1
-        cv2.imwrite(
-            f"AlbumentationAugments/{name}/{name}_augmented_{count}.png", augmented_image5)
-        count += 1
+# count = 0
+# for name in nameList:
+#     count = 0
+#     for i in range(0, 100, 5):
+#         image = cv2.imread("Dataset/"+name+"/"+name+"_"+str(i)+".png")
+#         augmented_image = transform(image=image)["image"]
+#         augmented_image2 = transform(image=image)["image"]
+#         augmented_image3 = transform(image=image)["image"]
+#         augmented_image4 = transform(image=image)["image"]
+#         augmented_image5 = transform(image=image)["image"]
+
+#         cv2.imwrite(
+#             f"AlbumentationAugments/{name}/{name}_augmented_{count}.png", augmented_image)
+#         count += 1
+#         cv2.imwrite(
+#             f"AlbumentationAugments/{name}/{name}_augmented_{count}.png", augmented_image2)
+#         count += 1
+#         cv2.imwrite(
+#             f"AlbumentationAugments/{name}/{name}_augmented_{count}.png", augmented_image3)
+#         count += 1
+#         cv2.imwrite(
+#             f"AlbumentationAugments/{name}/{name}_augmented_{count}.png", augmented_image4)
+#         count += 1
+#         cv2.imwrite(
+#             f"AlbumentationAugments/{name}/{name}_augmented_{count}.png", augmented_image5)
+#         count += 1
+
+train_data = datasets.ImageFolder('TrainingData/train', transform=transform)
+val_data = datasets.ImageFolder('TrainingData/val', transform=transform)
+train_loader = torch.utils.data.DataLoader(
+    train_data, batch_size=32, shuffle=True)
+val_loader = torch.utils.data.DataLoader(val_data, batch_size=32)
+model = models.resnet18(pretrained=True)
+
